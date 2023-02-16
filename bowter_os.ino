11214814dcbcb8c17deb80c7766b90be20d6244d
Jonas ULveseth <jonas@bowter.com>
2023-02-16 10:47:38 +0100

    first commit

---
+++ bowter_os.ino
@@ -0,0 +1,81 @@
+// This example uses an ESP32 Development Board
+// to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://www.shiftr.io/try.
+//
+// by Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
+
+#include <WiFiClientSecure.h>
+#include <MQTT.h>
+
+const char ssid[] = "ulveseth_Guest";
+const char pass[] = "pass";
+
+WiFiClientSecure net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  // do not verify tls certificate
+  // check the following example for methods to verify the server:
+  // https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFiClientSecure/examples/WiFiClientSecure/WiFiClientSecure.ino
+  net.setInsecure();
+  while (!client.connect("arduino", "kfbvYA.b4cxoA", "gJ5v9EaxHRSyFJ5ZhOMx0DYkFY6rew84UXvzTNxD1zE")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("1");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+
+  // Note: Do not use the client in the callback to publish, subscribe or
+  // unsubscribe as it may cause deadlocks when other things arrive while
+  // sending and receiving acknowledgments. Instead, change a global variable,
+  // or push to a queue and handle it in the loop after calling `client.loop()`.
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid);
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported
+  // by Arduino. You need to set the IP address directly.
+  //
+  // MQTT brokers usually use port 8883 for secure connections.
+  client.begin("mqtt.ably.io", 8883, net);
+  client.onMessage(messageReceived);
+
+  connect();
+}
+
+void loop() {
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
+    Serial.println("\nconnected!");
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 5000) {
+    lastMillis = millis();
+    //client.publish("1", "100%");
+  }
+}
